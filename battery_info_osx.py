77a20cdb9cfc5c875cd1fd048403c0fd6869e13f
Ivan I. Iliev <iai@melexis.com>
2017-08-31 11:52:59 +0300

    Modified the battery_info_osx.py script, playing with github

--- battery_info_osx.py
+++ battery_info_osx.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 import os
-os.system("system_profiler SPPowerDataType")
+#os.system("system_profiler SPPowerDataType")
+
+macbook_pro_year = 2011
+place_of_birth = "California"
+
+print("This is some text that is going to be printed!")
+
+print("My MacBook Pro was assembled in", place_of_birth, "in the year", macbook_pro_year,)
